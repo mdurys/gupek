be6312001d2b1a9f6c697fa0a1033fd75278ff3c
Michał Durys <michal@durys.pl>
2014-11-01 19:04:36 +0100

    Added Meeting entity

---
+++ src/MDurys/GupekBundle/Entity/Meeting.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace MDurys\GupekBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Meeting
+ *
+ * @ORM\Table(name="meetings")
+ * @ORM\Entity(repositoryClass="MDurys\GupekBundle\Entity\MeetingRepository")
+ */
+class Meeting
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     * @Assert\NotBlank()
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Meeting
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
