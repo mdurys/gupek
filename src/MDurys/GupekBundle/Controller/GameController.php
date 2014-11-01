500cc8b68b0375e436f8a87f9c1fa4c8978e621f
Michał Durys <michal@durys.pl>
2014-11-01 23:33:02 +0100

    fix

--- src/MDurys/GupekBundle/Controller/GameController.php
+++ src/MDurys/GupekBundle/Controller/GameController.php
@@ -10,7 +10,7 @@ class GameController extends Controller
     /**
      * @Route("/game")
      */
-    public function indexAction($name)
+    public function indexAction()
     {
         return $this->render('MDurysGupekBundle:Game:index.html.twig', []);
     }
