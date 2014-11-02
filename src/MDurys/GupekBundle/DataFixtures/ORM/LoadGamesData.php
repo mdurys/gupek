b922709ded8c31bc719019c71b7de47c169c6d71
Michał Durys <michal@durys.pl>
2014-11-02 20:03:38 +0100

    more games in fixtures

--- src/MDurys/GupekBundle/DataFixtures/ORM/LoadGamesData.php
+++ src/MDurys/GupekBundle/DataFixtures/ORM/LoadGamesData.php
@@ -17,6 +17,11 @@ class LoadGamesData extends AbstractFixture implements OrderedFixtureInterface
         $data = [
             'waterdeep' => ['Lords of Waterdeep', 2, 5],
             'catan' => ['Osadnicy z Catanu', 2, 4],
+            'carson' => ['Carson City', 2, 5],
+            'tikal' => ['Tikal', 2, 4],
+            'merchants' => ['Merchants of the Middle Ages', 2, 4],
+            'pokolenia' => ['Pokolenia', 2, 4],
+            'imperial2030' => ['Imperial 2030', 2, 6],
         ];
 
         foreach ($data as $id => $row) {
