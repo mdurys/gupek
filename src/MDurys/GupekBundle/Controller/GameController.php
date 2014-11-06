df9d55b0335bcb25010fa57bbcec3ca69783307c
Michał Durys <michal@durys.pl>
2014-11-06 11:25:41 +0100

    Added dummy game/show action

--- src/MDurys/GupekBundle/Controller/GameController.php
+++ src/MDurys/GupekBundle/Controller/GameController.php
@@ -4,14 +4,26 @@ namespace MDurys\GupekBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use MDurys\GupekBundle\Entity\Game;
 
 class GameController extends Controller
 {
     /**
      * @Route("/game")
+     * @Method("GET")
      */
     public function indexAction()
     {
         return $this->render('MDurysGupekBundle:Game:index.html.twig', []);
+    }
+
+    /**
+     * @Route("/game/{slug}")
+     * @Method("GET")
+     */
+    public function showAction(Game $game)
+    {
+        return $this->render('MDurysGupekBundle:Game:show.html.twig', ['game' => $game]);
     }
 }
