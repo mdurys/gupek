978858141aa55b99244f281eb57fa485ed46baca
Michał Durys <michal@durys.pl>
2014-11-29 21:24:58 +0100

    Sort games in fixtures

--- src/MDurys/GupekBundle/DataFixtures/ORM/LoadGamesData.php
+++ src/MDurys/GupekBundle/DataFixtures/ORM/LoadGamesData.php
@@ -15,21 +15,21 @@ class LoadGamesData extends AbstractFixture implements OrderedFixtureInterface
     public function load(ObjectManager $em)
     {
         $data = [
-            'waterdeep' => ['Lords of Waterdeep', 2, 5],
-            'catan' => ['Osadnicy z Catanu', 2, 4],
             'carson' => ['Carson City', 2, 5],
-            'tikal' => ['Tikal', 2, 4],
+            'catan' => ['Osadnicy z Catanu', 2, 4],
             'finca' => ['Finca', 2, 4],
-            'merchants' => ['Merchants of the Middle Ages', 2, 4],
-            'pokolenia' => ['Pokolenia', 2, 4],
-            'imperial2030' => ['Imperial 2030', 2, 6],
-            'samuraj' => ['Samuraj', 2, 4],
             'hacienda' => ['Hacienda', 2, 5],
-            'navigator' => ['Navigator', 2, 5],
-            'rheinlander' => ['Rheinländer', 3, 5],
-            'mumia' => ['Mumia — Wyścig w bandażach', 2, 6],
-            'wysokie_napiecie' => ['Wysokie napięcie', 2, 6],
+            'imperial2030' => ['Imperial 2030', 2, 6],
             'keyflower' => ['Keyflower', 2, 6],
+            'merchants' => ['Merchants of the Middle Ages', 2, 4],
+            'mumia' => ['Mumia — Wyścig w bandażach', 2, 6],
+            'navigator' => ['Navigator', 2, 5],
+            'pokolenia' => ['Pokolenia', 2, 4],
+            'rheinlander' => ['Rheinländer', 3, 5],
+            'samuraj' => ['Samuraj', 2, 4],
+            'tikal' => ['Tikal', 2, 4],
+            'waterdeep' => ['Lords of Waterdeep', 2, 5],
+            'wysokie_napiecie' => ['Wysokie napięcie', 2, 6],
         ];
 
         foreach ($data as $id => $row) {
