c9975560f0183b1640fc41096248bc20191d5af2
Michał Durys <michal@durys.pl>
2015-07-11 22:42:42 +0200

    Added MeetingUserTypeTest

---
+++ src/MDurys/GupekBundle/Tests/Form/MeetingUserTypeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MDurys\GupekBundle\Tests\Form;
+
+use Symfony\Component\Form\Test\TypeTestCase;
+use MDurys\GupekBundle\Form\MeetingUserType;
+
+class MeetingUserTypeTest extends TypeTestCase
+{
+    public function testSubmitValidData()
+    {
+        $formData = [
+            'place' => 2,
+            ];
+
+        $type = new MeetingUserType();
+        $form = $this->factory->create($type);
+
+        // submit the data to the form directly
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertInstanceOf('MDurys\GupekBundle\Entity\MeetingUser', $form->getData());
+
+        $view = $form->createView();
+        $children = $view->children;
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
