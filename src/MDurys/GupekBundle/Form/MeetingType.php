9e37d4809e09b01baaa4e1c4c98d321ba7de3c5d
Michał Durys <michal@durys.pl>
2015-11-04 10:29:22 +0100

    Meeting creation: limited minutes choice to 0, 15, 30 and 45. [ci skip]

--- src/MDurys/GupekBundle/Form/MeetingType.php
+++ src/MDurys/GupekBundle/Form/MeetingType.php
@@ -15,7 +15,7 @@ class MeetingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date', 'datetime', ['label' => 'form.meeting.date'])
+            ->add('date', 'datetime', ['label' => 'form.meeting.date', 'minutes' => [0, 15, 30, 45]])
             ->add('season')
         ;
     }
