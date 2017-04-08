dc61cba88074c0c7236e37fe5813df8936d8a71e
Michał Durys <michal@durys.pl>
2017-04-08 22:41:33 +0200

    Added methods for adding and removing players to meeting

--- src/MDurys/GupekBundle/Domain/Meeting/MeetingInterface.php
+++ src/MDurys/GupekBundle/Domain/Meeting/MeetingInterface.php
@@ -2,6 +2,8 @@
 
 namespace MDurys\GupekBundle\Domain\Meeting;
 
+use MDurys\GupekBundle\Domain\Player\PlayerInterface;
+
 interface MeetingInterface
 {
     /**
@@ -13,4 +15,18 @@ interface MeetingInterface
      * @return \DateTime
      */
     public function getDate(): \DateTime;
+
+    /**
+     * @param PlayerInterface $player
+     *
+     * @return bool
+     */
+    public function addPlayer(PlayerInterface $player): bool;
+
+    /**
+     * @param PlayerInterface $player
+     *
+     * @return bool
+     */
+    public function removePlayer(PlayerInterface $player): bool;
 }
