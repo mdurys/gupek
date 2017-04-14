d4b6e5e7609e0883e6f011d2ea7ace78fd791203
Michał Durys <michal@durys.pl>
2017-04-14 16:30:51 +0200

    Added MeetingRepositoryInterface::getBySeason

--- src/MDurys/GupekBundle/Domain/Meeting/MeetingRepositoryInterface.php
+++ src/MDurys/GupekBundle/Domain/Meeting/MeetingRepositoryInterface.php
@@ -2,7 +2,17 @@
 
 namespace MDurys\GupekBundle\Domain\Meeting;
 
-
+/**
+ * Interface MeetingRepositoryInterface
+ * @package MDurys\GupekBundle\Domain\Meeting
+ */
 interface MeetingRepositoryInterface
 {
+    /**
+     * Get meetings from given season.
+     *
+     * @param SeasonInterface $season
+     * @return MeetingInterface[]
+     */
+    public function getBySeason(SeasonInterface $season): array;
 }
