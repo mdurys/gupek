b4720fc66c5580ca985b2502528a007ce7597442
Michał Durys <michal@durys.pl>
2017-09-25 20:58:42 +0200

    Fixed Symfony 3.4 deprecation warnings

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !preg_match('/^(127\.0\.0\.1|10\.48\.100\.\d{1,3}|192\.168\.1\.\d{1,3}|172\.22\.2\.\d{1,3}|fe80::1|::1)$/', @$_SERVER['REMOTE_ADDR'])
+    || !preg_match('/^(127\.0\.0\.1|10\.0\.3\.\d{1,3}|10\.48\.100\.\d{1,3}|192\.168\.1\.\d{1,3}|172\.22\.2\.\d{1,3}|fe80::1|::1)$/', @$_SERVER['REMOTE_ADDR'])
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
@@ -23,7 +23,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
