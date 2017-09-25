c74cd57b600ce04f7f542c3fd60a2bb1d66a23e7
Michał Durys <michal@durys.pl>
2017-09-25 23:09:50 +0200

    Defined Bout statuses

--- src/MDurys/GupekBundle/Domain/Meeting/BoutInterface.php
+++ src/MDurys/GupekBundle/Domain/Meeting/BoutInterface.php
@@ -1,11 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MDurys\GupekBundle\Domain\Meeting;
 
 use MDurys\GupekBundle\Domain\Player\PlayerInterface;
 
 interface BoutInterface
 {
+    /**
+     * @var int Bout was newly added
+     */
+    const NEW = 0;
+
+    /**
+     * @var int Bout was finished and can be scored
+     */
+    const FINISHED = 1;
+
+    /**
+     * @var int Bout was aborted and will not be scored
+     */
+    const ABORTED = 2;
+
     /**
      * @param PlayerInterface $player
      *
